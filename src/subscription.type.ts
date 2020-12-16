962d531a77fda88517433a01e0ee208aa705dcf3
Hazem Khaled <hazem.khaled@gmail.com>
2020-12-16 18:21:51 +0300

    subscriptions

---
+++ src/subscription.type.ts
@@ -0,0 +1,21 @@
+/**
+ * User subscription
+ *
+ * @export
+ * @interface Subscription
+ */
+export interface Subscription {
+  id: string;
+  membershipId: string;
+  storeId: string;
+  invoiceId: string;
+  reference: string;
+
+  status: "confirmed" | "pending" | "cancelled";
+
+  startDate: Date;
+  expireDate: Date;
+  autoRenew: boolean;
+  renewed: boolean;
+  retries: Date[];
+}
