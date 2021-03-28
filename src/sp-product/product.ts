a27285ec17724f7ea42b17c4806e808e72de3f05
cactusaurelius <39528088+cactusaurelius@users.noreply.github.com>
2021-03-28 18:52:26 +0300

    Types refactoring and optionals

--- src/sp-product/product.ts
+++ src/sp-product/product.ts
@@ -4,21 +4,22 @@ import { SPCategory } from "../sp-category";
 import { SPProductVariation } from "./variation";
 
 export interface SPProduct {
-  attributes: ProductAttribute[];
-  barcode: string;
+  attributes?: ProductAttribute[];
+  external_id?: string;
+  barcode?: string;
   brand: I18nString;
-  categories: SPCategory[];
-  created_date: Date;
-  description: I18nString;
-  description_short: I18nString;
+  categories?: SPCategory[];
+  created_date?: Date;
+  description?: I18nString;
+  description_short?: I18nString;
   images: string[];
   name: I18nString;
-  qualified: 1 | 2 | 4 | 5;
+  qualified?: 1 | 2 | 4 | 5;
   sku: string;
-  source_sku: string;
-  supplier_id: string;
-  updated_date: Date;
+  source_sku?: string;
+  supplier_id?: string;
+  updated_date?: Date;
   url: string;
   variations: SPProductVariation[];
-  warehouse_id: string;
+  warehouse_id?: string;
 }
