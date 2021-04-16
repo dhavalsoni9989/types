0575c60b51e9296cd57e3c25472468a590342ef1
Dhaval Dhrangadhariya <dhavalsoni9989@gmail.com>
2021-04-16 17:45:53 +0530

    fix: Resolved the taxes type issues

--- src/tax.ts
+++ src/tax.ts
@@ -1,8 +1,9 @@
-export interface Tax {
+export interface Tax<T = { [key: string]: string | number }> {
   class: string[];
   country: string;
   id: string;
   isInclusive: boolean;
+  metadata?: T;
   name: string;
   omsId: string;
   percentage: number;
